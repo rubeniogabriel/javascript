a5caa1c7839c71db26467e4ffe569683017b1bc0
Rubênio Gabriel <rubeniogabriel10@gmail.com>
2017-04-12 23:34:53 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,9 @@
+function evento_entrar(){
+	alert("Seja bem vindo);
+}
+function evento_sair(){
+	alert("Até a próxima visita");
+}
+function evento_clique(){
+	document.getElementByID("id2").style.color = "red";
+}
